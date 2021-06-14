2c84183bc30987b843d8a71647957c5bd112922e
zakirayubov <zakirayubov@mail.ru>
2021-06-14 18:34:40 +0300

    hw4-collections

---
+++ hw4-list/src/main/java/ru/otus/CustomerReverseOrder.java
@@ -0,0 +1,20 @@
+package ru.otus;
+
+
+import java.util.Stack;
+
+public class CustomerReverseOrder {
+
+    //todo: 2. надо реализовать методы этого класса
+    //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+
+    private Stack<Customer> stack = new Stack<>();
+
+    public void add(Customer customer) {
+        stack.add(customer);
+    }
+
+    public Customer take() {
+        return stack.pop();
+    }
+}
